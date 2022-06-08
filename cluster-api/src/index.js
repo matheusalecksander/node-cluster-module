b5995cf4fb471d46c7d0f04f7c6060d3fb6b9a0a
matheusalecksander <projetos.matheusalecksander@gmail.com>
2022-06-08 15:56:22 -0300

    feat: created method to up an new worker if our server has exited

--- cluster-api/src/index.js
+++ cluster-api/src/index.js
@@ -9,6 +9,13 @@ const runPrimaryProcess = () => {
 
   for(var i = 0; i < processesCount; i++)
     cluster.fork()
+
+  cluster.on('exit', (worker, code, signal) => {
+    if(code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(`Worker ${worker.process.pid} died... scheduling another one`)
+      cluster.fork()
+    }
+  })
 }
 
 const runWorkerProcess = async () => {
